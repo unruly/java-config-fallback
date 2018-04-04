49a80b11d5425179e2131d9904f9f8b2183a8c95
SupplySideDevelopers <supplyside@unrulygroup.com>
2018-04-04 13:28:39 +0000

    feat(project setup): Implement configuration source for incode map and configuration file.
    Co-authored-by: Alex Wilson <a.wilson@alumni.warwick.ac.uk>
    Co-authored-by: Mohamed Osman <trc5019@gmail.com>

---
+++ src/main/java/co/unruly/config/Configuration.java
@@ -0,0 +1,59 @@
+package co.unruly.config;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Properties;
+
+public class Configuration {
+
+    private ConfigurationSource func = (key) -> null;
+
+    public Configuration(ConfigurationSource map) {
+        this.func = map;
+    }
+
+    public static Configuration from(ConfigurationSource func) {
+        return new Configuration(func);
+    }
+
+    public Optional<?> get(String s) {
+        return Optional.ofNullable(func.get(s));
+    }
+
+    public Configuration or(ConfigurationSource File) {
+        this.func = this.func.or(File);
+        return this;
+    }
+
+    public static ConfigurationSource map(Map<String, String> map){
+        return map::get;
+    };
+
+    public static ConfigurationSource properties(String s) {
+        Properties properties = new Properties();
+
+        try {
+            properties.load(new FileReader(s));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return properties::getProperty;
+    }
+}
+
+
+interface ConfigurationSource {
+
+    Object get(String key);
+
+    default ConfigurationSource or(ConfigurationSource source) {
+        return (key) -> {
+            return Optional
+                .ofNullable(this.get(key))
+                .orElse(source.get(key));
+        };
+    }
+}
---
+++ src/test/java/co/unruly/config/ConfigurationTest.java
@@ -0,0 +1,49 @@
+package co.unruly.config;
+
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static co.unruly.config.Configuration.map;
+import static co.unruly.config.Configuration.properties;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigurationTest {
+
+    @Test
+    public void shouldReturnEmptyOptionalIfValueNotFound() {
+        Map<String, String> map = Collections.emptyMap();
+
+        Configuration config = Configuration.from(map::get);
+
+        assertThat(config.get("some-variable"), is(Optional.empty()));
+    }
+
+    @Test
+    public void shouldReturnOptionalIfValueIsPresent() {
+        Map<String, String> input = new HashMap<>();
+
+        input.put("some-variable", "dfsadad");
+
+        Configuration config = Configuration.from(input::get);
+
+        assertThat(config.get("some-variable"), is(Optional.of("dfsadad")));
+    }
+
+    @Test
+    public void shouldReadFromFileFirstIfValueIsPresent() {
+        Map<String, String> input = new HashMap<>();
+        input.put("some-variable", "dfsadad");
+
+        Configuration config = Configuration
+            .from(properties("src/test/resources/test.properties"))
+            .or(map(input));
+
+        assertThat(config.get("some-variable"), is(Optional.of("blah")));
+    }
+
+}
